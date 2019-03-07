534a5434407f0c84ac3575589cf7b48df34b2256
winton <mail@wintoni.us>
2019-03-07 02:40:58 -0500

    Simplify logic

--- alias.js
+++ alias.js
@@ -1,55 +1,46 @@
 /*prettier-ignore*/
 "use strict"
 
-module.exports = function(dot, opts) {
-  var state = dot.state
-
-  if (state.alias) {
+module.exports = function(dot) {
+  if (dot.alias) {
     return
   }
 
-  state.alias = opts || {}
-  state.parsedAlias = {}
+  dot.state.alias = {}
 
   dot.any("alias", alias)
 }
 
 function alias(prop, arg, dot) {
   dot.state.alias[prop[0]] = arg
-  dot.any(prop, aliasArgs)
+  dot.any(prop[0], aliasArgs)
 }
 
 function aliasArgs(prop, arg, dot, eventId) {
   var state = dot.state
   var alias = state.alias
-  var parsed = state.parsedAlias
 
   if (!alias[eventId]) {
     return
   }
 
-  if (!parsed[eventId]) {
-    parsed[eventId] = parseAlias(alias[eventId])
-  }
+  var eventAlias = alias[eventId]
 
-  var argCopy = Object.assign({}, arg),
-    p = parsed[eventId]
-
-  for (var key in arg) {
-    if (!p[key]) {
+  for (var key in eventAlias) {
+    if (!eventAlias[key]) {
       continue
     }
 
-    var keys = p[key].concat([key]).sort()
+    var keys = eventAlias[key].concat([key]).sort()
 
     var value = keys.reduce(function(memo, k) {
-      if (Array.isArray(argCopy[k])) {
+      if (Array.isArray(arg[k])) {
         memo = !memo || Array.isArray(memo) ? memo : [memo]
-        return argCopy[k].concat(memo || [])
-      } else if (typeof argCopy[k] === "object") {
-        return Object.assign(memo || {}, argCopy[k])
-      } else if (argCopy[k]) {
-        return argCopy[k]
+        return arg[k].concat(memo || [])
+      } else if (typeof arg[k] === "object") {
+        return Object.assign(memo || {}, arg[k])
+      } else if (arg[k]) {
+        return arg[k]
       } else {
         return memo
       }
@@ -59,32 +50,4 @@ function aliasArgs(prop, arg, dot, eventId) {
       arg[k] = value
     })
   }
-}
-
-function parseAlias(aliases) {
-  var alias,
-    any,
-    i,
-    k,
-    key,
-    len,
-    out = {},
-    prev
-
-  for (key in aliases) {
-    any = aliases[key]
-    alias = out[key] = Array.isArray(any) ? any : [any]
-
-    for (i = 0, len = alias.length; i < len; i++) {
-      prev = out[alias[i]] = [key]
-
-      for (k = 0; k < len; k++) {
-        if (i !== k) {
-          prev.push(alias[k])
-        }
-      }
-    }
-  }
-
-  return out
 }
